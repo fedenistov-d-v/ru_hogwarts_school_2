08fb160dc31d35cf7c36e4b340d1710d2b0c8687
fdmitriy <fdmitriy@inbox.ru>
2025-12-14 22:18:49 +0300

    Введение в базы данных. Всё работает!!!

--- src/main/java/my/hogwarts/school/model/Student.java
+++ src/main/java/my/hogwarts/school/model/Student.java
@@ -1,8 +1,6 @@
 package my.hogwarts.school.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
